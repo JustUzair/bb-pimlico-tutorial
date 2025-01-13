072731ca1b5edd91bfd0942f573fc9923c2340c8
JustUzair <uzairhajra763390@gmail.com>
2025-01-13 12:03:08 +0530

    Updated code to simulate polygon mainnet and send tx to buildbear polygon mainnet fork

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { appendFileSync, writeFileSync } from "fs";
 import { toSafeSmartAccount } from "permissionless/accounts";
 import { Hex, createPublicClient, defineChain, getContract, http } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-import { polygon } from "viem/chains";
+// import { polygon } from "viem/chains";
 import { createPimlicoClient } from "permissionless/clients/pimlico";
 import {
   createBundlerClient,
@@ -15,9 +15,32 @@ import { getAddress, maxUint256, parseAbi } from "viem";
 import { EntryPointVersion } from "viem/account-abstraction";
 
 import { encodeFunctionData, parseAbiItem } from "viem";
+import { ethers, getBigInt } from "ethers";
 
 const apiKey = process.env.PIMLICO_API_KEY;
+const pimlicoUrl = `https://api.pimlico.io/v2/137/rpc?apikey=${apiKey}`; // @>>> Get the api key and rpc from pimlico dashboard
+
+const buildbearSandboxUrl =
+  "https://rpc.dev.buildbear.io/national-scarletwitch-c11333f2";
 if (!apiKey) throw new Error("Missing PIMLICO_API_KEY");
+export const BBSandboxNetwork = /*#__PURE__*/ defineChain({
+  id: 137,
+  name: "BB",
+  nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.dev.buildbear.io/national-scarletwitch-c11333f2"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "BB National Scarletwitch Scan",
+      url: "https://explorer.dev.buildbear.io/national-scarletwitch-c11333f2",
+      apiUrl:
+        "https://https://api.dev.buildbear.io/national-scarletwitch-c11333f2/api",
+    },
+  },
+});
 
 const privateKey = (process.env.PRIVATE_KEY as Hex)
   ? (process.env.PRIVATE_KEY as Hex)
@@ -28,14 +51,12 @@ const privateKey = (process.env.PRIVATE_KEY as Hex)
     })();
 
 export const publicClient = createPublicClient({
-  chain: polygon,
+  chain: BBSandboxNetwork,
   transport: http(
-    "https://rpc.dev.buildbear.io/national-scarletwitch-c11333f2"
+    buildbearSandboxUrl
     // "https://endpoints.omniatech.io/v1/matic/mainnet/public"
   ), //@>>> Put in buildbear rpc
 });
-
-const pimlicoUrl = `https://api.pimlico.io/v2/137/rpc?apikey=${apiKey}`; // @>>> Get the api key and rpc from pimlico dashboard
 
 const pimlicoClient = createPimlicoClient({
   transport: http(pimlicoUrl),
@@ -55,10 +76,12 @@ const account = await toSafeSmartAccount({
   version: "1.4.1",
 });
 
+console.log("\n\n====================================");
 console.log("====================================");
 console.log(
   `Smart account address: https://explorer.dev.buildbear.io/national-scarletwitch-c11333f2/address/${account.address}`
 );
+console.log("====================================");
 console.log("====================================");
 
 // console.log("====================================");
@@ -69,20 +92,37 @@ console.log("====================================");
 
 const smartAccountClient = createSmartAccountClient({
   account,
-  chain: polygon,
-  bundlerTransport: http(pimlicoUrl),
+  chain: BBSandboxNetwork,
+  bundlerTransport: http(buildbearSandboxUrl), //sending the tx to buildbear
   paymaster: pimlicoClient,
-  userOperation: {
-    estimateFeesPerGas: async () => {
-      return (await pimlicoClient.getUserOperationGasPrice()).fast;
-    },
-  },
+  // userOperation: {
+  //   estimateFeesPerGas: async () => {
+  //     return (await pimlicoClient.getUserOperationGasPrice()).fast;
+  //   },
+  // },
 });
 
-const txHash = await smartAccountClient.sendTransaction({
-  to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-  value: 0n,
-  data: "0x1234",
+const txHash = await smartAccountClient.sendUserOperation({
+  sender: "0x5a6b47F4131bf1feAFA56A05573314BcF44C9149",
+  nonce: getBigInt(
+    "0x845ADB2C711129D4F3966735ED98A9F09FC4CE5700000000000000000000"
+  ),
+  factory: "0xd703aaE79538628d27099B8c4f621bE4CCd142d5",
+  factoryData:
+    "0xc5265d5d000000000000000000000000aac5d4240af87249b3f71bc8e4a2cae074a3e4190000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001243c3b752b01845ADb2C711129d4f3966735eD98a9F09fC4cE570000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000014375d883Cb4afb913aC35c4B394468C4bC73d77C40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+  callData:
+    "0xe9ae5c5300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000003400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+  callGasLimit: getBigInt("0x13880"),
+  verificationGasLimit: getBigInt("0x60B01"),
+  preVerificationGas: getBigInt("0xD3E3"),
+  maxPriorityFeePerGas: getBigInt("0x3B9ACA00"),
+  maxFeePerGas: getBigInt("0x7A5CF70D5"),
+  // paymaster: pimlicoClient.account?.address,
+  // paymasterVerificationGasLimit: getBigInt("0x0"),
+  // paymasterPostOpGasLimit: getBigInt("0x0"),
+  // paymasterData: "0x",
+  signature:
+    "0xa6cc6589c8bd561cfd68d7b6b0757ef6f208e7438782939938498eee7d703260137856c840c491b3d415956265e81bf5c2184a725be2abfc365f7536b6af525e1c",
 });
 
 console.log(
