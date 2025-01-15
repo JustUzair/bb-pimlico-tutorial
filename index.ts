0efeb45713803a93cd77b327905726890d1c6f62
JustUzair <uzairhajra763390@gmail.com>
2025-01-15 18:11:06 +0530

    pending debugging

--- index.ts
+++ index.ts
@@ -15,6 +15,7 @@ import { getAddress, maxUint256, parseAbi } from "viem";
 import { EntryPointVersion } from "viem/account-abstraction";
 
 import { encodeFunctionData, parseAbiItem } from "viem";
+
 import { ethers, getBigInt } from "ethers";
 
 const apiKey = process.env.PIMLICO_API_KEY;
@@ -23,6 +24,7 @@ const pimlicoUrl = `https://api.pimlico.io/v2/137/rpc?apikey=${apiKey}`; // @>>>
 const buildbearSandboxUrl =
   "https://rpc.dev.buildbear.io/rival-deadpool-e1ac7a6e";
 if (!apiKey) throw new Error("Missing PIMLICO_API_KEY");
+
 export const BBSandboxNetwork = /*#__PURE__*/ defineChain({
   id: 137,
   name: "BB",
@@ -92,38 +94,60 @@ console.log("====================================");
 const smartAccountClient = createSmartAccountClient({
   account,
   chain: BBSandboxNetwork,
-  bundlerTransport: http(buildbearSandboxUrl), //sending the tx to buildbear
-  paymaster: pimlicoClient,
-  // userOperation: {
-  //   estimateFeesPerGas: async () => {
-  //     return (await pimlicoClient.getUserOperationGasPrice()).fast;
-  //   },
-  // },
+  bundlerTransport: http(
+    "https://rpc.dev.buildbear.io/rival-deadpool-e1ac7a6e"
+  ), //sending the tx to buildbear
+  // paymaster: pimlicoClient,
+  userOperation: {
+    estimateFeesPerGas: async () => {
+      return (await pimlicoClient.getUserOperationGasPrice()).fast;
+    },
+  },
 });
+
+// const txHash = await smartAccountClient.sendTransaction({
+//   sender: account.address,
+//   nonce: getBigInt(
+//     "0x845ADB2C711129D4F3966735ED98A9F09FC4CE5700000000000000000000"
+//   ),
+//   factory: "0xd703aaE79538628d27099B8c4f621bE4CCd142d5",
+//   factoryData:
+//     "0xc5265d5d000000000000000000000000aac5d4240af87249b3f71bc8e4a2cae074a3e4190000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001243c3b752b01845ADb2C711129d4f3966735eD98a9F09fC4cE570000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000014375d883Cb4afb913aC35c4B394468C4bC73d77C40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+//   callData:
+//     "0xe9ae5c5300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000003400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+//   callGasLimit: getBigInt("0x13880"),
+//   verificationGasLimit: getBigInt("0x60B01"),
+//   preVerificationGas: getBigInt("0xD3E3"),
+//   maxPriorityFeePerGas: getBigInt("0x3B9ACA00"),
+//   maxFeePerGas: getBigInt("0x905ae1380"),
+//   // paymaster: pimlicoClient.account?.address,
+//   // paymasterVerificationGasLimit: getBigInt("0x0"),
+//   // paymasterPostOpGasLimit: getBigInt("0x0"),
+//   // paymasterData: "0x",
+//   signature:
+//     "0xa6cc6589c8bd561cfd68d7b6b0757ef6f208e7438782939938498eee7d703260137856c840c491b3d415956265e81bf5c2184a725be2abfc365f7536b6af525e1c",
+// });
+console.log("====================================");
+console.log(getBigInt(100));
+console.log("====================================");
+
+// console.log("====================================");
+// console.log(account.signUserOperation());
+// console.log("====================================");
+
+// account.sign;
 
 const txHash = await smartAccountClient.sendUserOperation({
-  sender: "0x5a6b47F4131bf1feAFA56A05573314BcF44C9149",
-  nonce: getBigInt(
-    "0x845ADB2C711129D4F3966735ED98A9F09FC4CE5700000000000000000000"
-  ),
-  factory: "0xd703aaE79538628d27099B8c4f621bE4CCd142d5",
-  factoryData:
-    "0xc5265d5d000000000000000000000000aac5d4240af87249b3f71bc8e4a2cae074a3e4190000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001243c3b752b01845ADb2C711129d4f3966735eD98a9F09fC4cE570000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000014375d883Cb4afb913aC35c4B394468C4bC73d77C40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-  callData:
-    "0xe9ae5c5300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000003400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-  callGasLimit: getBigInt("0x13880"),
-  verificationGasLimit: getBigInt("0x60B01"),
-  preVerificationGas: getBigInt("0xD3E3"),
-  maxPriorityFeePerGas: getBigInt("0x3B9ACA00"),
-  maxFeePerGas: getBigInt("0x7A5CF70D5"),
-  // paymaster: pimlicoClient.account?.address,
-  // paymasterVerificationGasLimit: getBigInt("0x0"),
-  // paymasterPostOpGasLimit: getBigInt("0x0"),
-  // paymasterData: "0x",
-  signature:
-    "0xa6cc6589c8bd561cfd68d7b6b0757ef6f208e7438782939938498eee7d703260137856c840c491b3d415956265e81bf5c2184a725be2abfc365f7536b6af525e1c",
+  calls: [
+    {
+      to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+      value: ethers.parseUnits("100", "wei"),
+      data: "0x",
+    },
+  ],
+  // account,
 });
 
-console.log(
-  `User operation included: https://explorer.dev.buildbear.io/rival-deadpool-e1ac7a6e/tx/${txHash}`
-);
+// console.log(
+//   `User operation included: https://explorer.dev.buildbear.io/rival-deadpool-e1ac7a6e/tx/${txHash}`
+// );
