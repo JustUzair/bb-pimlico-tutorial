7b457a3c73d45c169f72a0f996b8bb5b0d200505
JustUzair <uzairhajra763390@gmail.com>
2025-01-15 21:38:58 +0530

    fixed implementation

--- index.ts
+++ index.ts
@@ -1,37 +1,31 @@
 import "dotenv/config";
 import { appendFileSync, writeFileSync } from "fs";
 import { toSafeSmartAccount } from "permissionless/accounts";
-import { Hex, createPublicClient, defineChain, getContract, http } from "viem";
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-// import { polygon } from "viem/chains";
-import { createPimlicoClient } from "permissionless/clients/pimlico";
 import {
-  createBundlerClient,
-  entryPoint07Address,
-} from "viem/account-abstraction";
+  Hex,
+  createPublicClient,
+  defineChain,
+  formatEther,
+  getContract,
+  http,
+} from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { createPimlicoClient } from "permissionless/clients/pimlico";
+import { entryPoint07Address } from "viem/account-abstraction";
 import { createSmartAccountClient } from "permissionless";
 
-import { getAddress, maxUint256, parseAbi } from "viem";
-import { EntryPointVersion } from "viem/account-abstraction";
-
-import { encodeFunctionData, parseAbiItem } from "viem";
-
-import { ethers, getBigInt } from "ethers";
-
-const apiKey = process.env.PIMLICO_API_KEY;
-const pimlicoUrl = `https://api.pimlico.io/v2/137/rpc?apikey=${apiKey}`; // @>>> Get the api key and rpc from pimlico dashboard
+import { ethers } from "ethers";
 
 const buildbearSandboxUrl =
   "https://rpc.dev.buildbear.io/rival-deadpool-e1ac7a6e";
-if (!apiKey) throw new Error("Missing PIMLICO_API_KEY");
 
 export const BBSandboxNetwork = /*#__PURE__*/ defineChain({
-  id: 137,
+  id: 11367,
   name: "BB",
   nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 },
   rpcUrls: {
     default: {
-      http: ["https://rpc.dev.buildbear.io/rival-deadpool-e1ac7a6e"],
+      http: [buildbearSandboxUrl],
     },
   },
   blockExplorers: {
@@ -53,23 +47,21 @@ const privateKey = (process.env.PRIVATE_KEY as Hex)
 
 export const publicClient = createPublicClient({
   chain: BBSandboxNetwork,
-  transport: http(
-    buildbearSandboxUrl
-    // "https://endpoints.omniatech.io/v1/matic/mainnet/public"
-  ), //@>>> Put in buildbear rpc
+  transport: http(buildbearSandboxUrl), //@>>> Put in buildbear rpc
 });
 
 const pimlicoClient = createPimlicoClient({
-  transport: http(pimlicoUrl),
+  transport: http(buildbearSandboxUrl),
   entryPoint: {
     address: entryPoint07Address,
     version: "0.7",
   },
 });
 
+const signer = privateKeyToAccount(privateKey);
 const account = await toSafeSmartAccount({
   client: publicClient,
-  owners: [privateKeyToAccount(privateKey)],
+  owners: [signer],
   entryPoint: {
     address: entryPoint07Address,
     version: "0.7",
@@ -77,26 +69,10 @@ const account = await toSafeSmartAccount({
   version: "1.4.1",
 });
 
-console.log("\n\n====================================");
-console.log("====================================");
-console.log(
-  `Smart account address: https://explorer.dev.buildbear.io/rival-deadpool-e1ac7a6e/address/${account.address}`
-);
-console.log("====================================");
-console.log("====================================");
-
-// console.log("====================================");
-// console.log(
-//   `Smart account address: https://polygonscan.com/address/${account.address}`
-// );
-// console.log("====================================");
-
 const smartAccountClient = createSmartAccountClient({
   account,
   chain: BBSandboxNetwork,
-  bundlerTransport: http(
-    "https://rpc.dev.buildbear.io/rival-deadpool-e1ac7a6e"
-  ), //sending the tx to buildbear
+  bundlerTransport: http(buildbearSandboxUrl), //sending the tx to buildbear
   // paymaster: pimlicoClient,
   userOperation: {
     estimateFeesPerGas: async () => {
@@ -105,49 +81,20 @@ const smartAccountClient = createSmartAccountClient({
   },
 });
 
-// const txHash = await smartAccountClient.sendTransaction({
-//   sender: account.address,
-//   nonce: getBigInt(
-//     "0x845ADB2C711129D4F3966735ED98A9F09FC4CE5700000000000000000000"
-//   ),
-//   factory: "0xd703aaE79538628d27099B8c4f621bE4CCd142d5",
-//   factoryData:
-//     "0xc5265d5d000000000000000000000000aac5d4240af87249b3f71bc8e4a2cae074a3e4190000000000000000000000000000000000000000000000000000000000000060000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001243c3b752b01845ADb2C711129d4f3966735eD98a9F09fC4cE570000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000a000000000000000000000000000000000000000000000000000000000000000e000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000014375d883Cb4afb913aC35c4B394468C4bC73d77C40000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-//   callData:
-//     "0xe9ae5c5300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000003400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-//   callGasLimit: getBigInt("0x13880"),
-//   verificationGasLimit: getBigInt("0x60B01"),
-//   preVerificationGas: getBigInt("0xD3E3"),
-//   maxPriorityFeePerGas: getBigInt("0x3B9ACA00"),
-//   maxFeePerGas: getBigInt("0x905ae1380"),
-//   // paymaster: pimlicoClient.account?.address,
-//   // paymasterVerificationGasLimit: getBigInt("0x0"),
-//   // paymasterPostOpGasLimit: getBigInt("0x0"),
-//   // paymasterData: "0x",
-//   signature:
-//     "0xa6cc6589c8bd561cfd68d7b6b0757ef6f208e7438782939938498eee7d703260137856c840c491b3d415956265e81bf5c2184a725be2abfc365f7536b6af525e1c",
-// });
-console.log("====================================");
-console.log(getBigInt(100));
-console.log("====================================");
+let balance = await publicClient.getBalance({ address: account.address }); // Get the balance of the sender
 
-// console.log("====================================");
-// console.log(account.signUserOperation());
-// console.log("====================================");
+console.log("Balance before transaction: ", formatEther(balance));
 
-// account.sign;
-
-const txHash = await smartAccountClient.sendUserOperation({
-  calls: [
-    {
-      to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-      value: ethers.parseUnits("100", "wei"),
-      data: "0x",
-    },
-  ],
-  // account,
+const txHash = await smartAccountClient.sendTransaction({
+  to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
+  value: ethers.parseEther("1"),
+  data: "0x",
 });
 
-// console.log(
-//   `User operation included: https://explorer.dev.buildbear.io/rival-deadpool-e1ac7a6e/tx/${txHash}`
-// );
+console.log(
+  `User operation included: https://explorer.dev.buildbear.io/rival-deadpool-e1ac7a6e/tx/${txHash}`
+);
+
+balance = await publicClient.getBalance({ address: account.address }); // Get the balance of the sender
+
+console.log("Balance after transaction: ", formatEther(balance));
