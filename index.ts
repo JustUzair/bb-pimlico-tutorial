bc15148393ffcfab222d22e06f89934e65ef5a67
JustUzair <uzairhajra763390@gmail.com>
2025-02-26 14:47:41 +0530

    Added modifications to support Pimlico Paymaster

--- index.ts
+++ index.ts
@@ -12,19 +12,18 @@ import {
 } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 import { createPimlicoClient } from "permissionless/clients/pimlico";
-import { entryPoint07Address } from "viem/account-abstraction";
+import { entryPoint07Address, UserOperation } from "viem/account-abstraction";
 import { createSmartAccountClient } from "permissionless";
 import { parseEther } from "ethers";
 import { exit } from "process";
 import ERC20Abi from "./utils/ABIs/ERC20.json";
 
-const buildbearSandboxUrl =
-  "https://rpc.buildbear.io/disturbed-bedlam-1060488b";
+const buildbearSandboxUrl = "https://rpc.dev.buildbear.io/uzair";
 
 const BBSandboxNetwork = /*#__PURE__*/ defineChain({
-  id: 23645, // IMPORTANT : replace this with your sandbox's chain id
+  id: 11476, // IMPORTANT : replace this with your sandbox's chain id
   name: "BuildBear x Polygon Mainnet Sandbox", // name your network
-  nativeCurrency: { name: "MATIC", symbol: "MATIC", decimals: 18 }, // native currency of forked network
+  nativeCurrency: { name: "BBETH", symbol: "BBETH", decimals: 18 }, // native currency of forked network
   rpcUrls: {
     default: {
       http: [buildbearSandboxUrl],
@@ -33,8 +32,8 @@ const BBSandboxNetwork = /*#__PURE__*/ defineChain({
   blockExplorers: {
     default: {
       name: "BuildBear x Polygon Mainnet Scan", // block explorer for network
-      url: "https://explorer.buildbear.io/disturbed-bedlam-1060488b",
-      apiUrl: "https://api.buildbear.io/disturbed-bedlam-1060488b/api",
+      url: "https://explorer.dev.buildbear.io/uzair",
+      apiUrl: "https://api.dev.buildbear.io/uzair/api",
     },
   },
 });
@@ -64,6 +63,7 @@ const signer = privateKeyToAccount(privateKey);
 const account = await toSafeSmartAccount({
   client: publicClient,
   owners: [signer],
+
   entryPoint: {
     address: entryPoint07Address,
     version: "0.7",
@@ -75,6 +75,7 @@ const smartAccountClient = createSmartAccountClient({
   account,
   chain: BBSandboxNetwork,
   bundlerTransport: http(buildbearSandboxUrl), //sending the tx to buildbear
+  paymaster: pimlicoClient,
   userOperation: {
     estimateFeesPerGas: async () => {
       return (await pimlicoClient.getUserOperationGasPrice()).fast;
@@ -84,7 +85,7 @@ const smartAccountClient = createSmartAccountClient({
 
 let balance = await publicClient.getBalance({ address: account.address }); // Get the balance of the sender
 let daiBalanceBefore = await getDAIBalance();
-let usdcBalanceBefore = await getUSDCBalance();
+let usdtBalanceBefore = await getUSDTBalance();
 
 if (+balance.toString() <= 0 || +daiBalanceBefore.toString() <= 0) {
   console.log("====================================");
@@ -102,16 +103,16 @@ if (+balance.toString() <= 0 || +daiBalanceBefore.toString() <= 0) {
 console.log("====================================");
 
 console.log(
-  "-------- UserOp to Swap DAI to USDC on Uniswap V3 with Alto ---------"
+  "-------- UserOp to Swap DAI to USDT on Uniswap V3 with Alto ---------"
 );
-console.log("🟠Balance before transaction: ", formatEther(balance));
-console.log("🟠DAI Balance before transaction: ", daiBalanceBefore);
-console.log("🟠USDC Balance before transaction: ", usdcBalanceBefore);
+console.log("🟠 Balance before transaction: ", formatEther(balance));
+console.log("🟠 DAI Balance before transaction: ", daiBalanceBefore);
+console.log("🟠 USDT Balance before transaction: ", usdtBalanceBefore);
 console.log("====================================");
 
 let swapParams = {
   tokenIn: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" as `0x${string}`, // DAI
-  tokenOut: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174" as `0x${string}`, // USDC
+  tokenOut: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F" as `0x${string}`, // USDT
   fee: 3000 as number, //fee
   recipient: account.address, // recipient
   deadline: (Math.floor(Date.now() / 1000) + 60 * 2) as unknown as bigint, // expiration
@@ -119,14 +120,74 @@ let swapParams = {
   amountOutMinimum: 0 as unknown as bigint, //amountOutMinimum
   sqrtPriceLimitX96: 0 as unknown as bigint, //sqrtPriceLimitX96
   v3Router: "0xE592427A0AEce92De3Edee1F18E0157C05861564" as `0x${string}`,
+  paymasterV7Address:
+    "0x0000000000000039cd5e8ae05257ce51c473ddd1" as `0x${string}`,
 };
 
 console.log("🟠 Approving DAI....");
 console.log("====================================");
 
+console.log("🟠 Calculating UserOp Cost in DAI....");
+const quotes = await pimlicoClient.getTokenQuotes({
+  chain: BBSandboxNetwork,
+  tokens: [swapParams.tokenIn],
+});
+const { postOpGas, exchangeRate, paymaster } = quotes[0];
+const userOperation: UserOperation<"0.7"> =
+  await smartAccountClient.prepareUserOperation({
+    calls: [
+      {
+        to: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" as `0x${string}`, //DAI
+        abi: parseAbi(["function approve(address,uint)"]),
+        functionName: "approve",
+        args: [swapParams.v3Router, parseEther("1")],
+      },
+      {
+        to: swapParams.v3Router, //UniV3 Router
+        abi: parseAbi([
+          "function exactInputSingle((address, address , uint24 , address , uint256 , uint256 , uint256 , uint160)) external payable returns (uint256 amountOut)",
+        ]),
+        functionName: "exactInputSingle",
+        args: [
+          [
+            swapParams.tokenIn,
+            swapParams.tokenOut,
+            swapParams.fee,
+            swapParams.recipient,
+            swapParams.deadline,
+            swapParams.amountIn,
+            swapParams.amountOutMinimum,
+            swapParams.sqrtPriceLimitX96,
+          ],
+        ],
+      },
+    ],
+  });
+const userOperationMaxGas =
+  userOperation.preVerificationGas +
+  userOperation.callGasLimit +
+  userOperation.verificationGasLimit +
+  (userOperation.paymasterPostOpGasLimit || 0n) +
+  (userOperation.paymasterVerificationGasLimit || 0n);
+
+const userOperationMaxCost = userOperationMaxGas * userOperation.maxFeePerGas;
+
+// using formula here https://github.com/pimlicolabs/singleton-paymaster/blob/main/src/base/BaseSingletonPaymaster.sol#L334-L341
+const maxCostInToken =
+  ((userOperationMaxCost + postOpGas * userOperation.maxFeePerGas) *
+    exchangeRate) /
+  BigInt(1e18);
+console.log("====================================");
+
 const txHash = await smartAccountClient.sendUserOperation({
   account,
   calls: [
+    {
+      to: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" as `0x${string}`, //DAI
+      abi: parseAbi(["function approve(address,uint)"]),
+      functionName: "approve",
+      args: [swapParams.paymasterV7Address, maxCostInToken],
+    },
     {
       to: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063" as `0x${string}`, //DAI
       abi: parseAbi(["function approve(address,uint)"]),
@@ -153,6 +214,9 @@ const txHash = await smartAccountClient.sendUserOperation({
       ],
     },
   ],
+  paymasterContext: {
+    token: swapParams.tokenIn,
+  },
 });
 console.log("🟠 Swapping DAI....");
 
@@ -163,31 +227,36 @@ let { receipt } = await smartAccountClient.waitForUserOperationReceipt({
 });
 
 console.log(
-  `🟢User operation included: https://explorer.buildbear.io/disturbed-bedlam-1060488b/tx/${receipt.transactionHash}`
+  `🟢User operation included: https://explorer.dev.buildbear.io/uzair/tx/${receipt.transactionHash}`
 );
 
 balance = await publicClient.getBalance({ address: account.address }); // Get the balance of the sender
 let daiBalanceAfter = await getDAIBalance();
-let usdcBalanceAfter = await getUSDCBalance();
+let usdtBalanceAfter = await getUSDTBalance();
 
 console.log(
   `🟢 Yay!! 🎉🎉 Swapped ${formatUnits(swapParams.amountIn, 18)} DAI to ${
-    +usdcBalanceAfter - +usdcBalanceBefore
-  } USDC`
+    +usdtBalanceAfter - +usdtBalanceBefore
+  } USDT`
 );
 
 console.log("🟢 Balance after transaction: ", formatEther(balance));
 console.log("🟢 DAI Balance after transaction: ", daiBalanceAfter);
-console.log("🟢 USDC Balance after transaction: ", usdcBalanceAfter);
+console.log("🟢 USDT Balance after transaction: ", usdtBalanceAfter);
+console.log("🟢 Max DAI Estimate for UserOp: ", formatEther(maxCostInToken));
+console.log(
+  "🟢 DAI charged for UserOp: ",
+  (+daiBalanceBefore - +daiBalanceAfter - 1).toFixed(18).replace(/\.?0+$/, "") // Adjust decimal places as needed
+);
 
 exit();
 
 // Helper Functions
 
-// get USDC Balance of Smart Account
-async function getUSDCBalance(): Promise<string> {
+// get USDT Balance of Smart Account
+async function getUSDTBalance(): Promise<string> {
   let res = await publicClient.readContract({
-    address: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
+    address: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
     abi: ERC20Abi,
     functionName: "balanceOf",
     args: [account.address],
